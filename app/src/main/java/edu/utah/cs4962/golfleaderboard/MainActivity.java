9307a84381b044747d598257018c066722b1bfd2
u0514185 <lj.johnson@path.utah.edu>
2014-11-18 12:08:32 -0700

    Changed create account button to a text view, made clickable, verified click with method attachment.

--- app/src/main/java/edu/utah/cs4962/golfleaderboard/MainActivity.java
+++ app/src/main/java/edu/utah/cs4962/golfleaderboard/MainActivity.java
@@ -1,31 +1,35 @@
 package edu.utah.cs4962.golfleaderboard;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity
 {
+
+    private EditText _username;
+    private EditText _password;
+    private Button _login;
+    private TextView _loginLockedTV;
+    private TextView _attemptsLeftTV;
+    private TextView _numberOfRemainingLoginAttemptsTV;
+    int _numberOfRemainingLoginAttempts = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null)
-        {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        setupVariables();
     }
 
 
@@ -57,7 +61,7 @@ public class MainActivity extends Activity
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment
+    /*public static class PlaceholderFragment extends Fragment
     {
 
         public PlaceholderFragment()
@@ -71,5 +75,48 @@ public class MainActivity extends Activity
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }*/
+
+    public void authenticateLogin(View view)
+    {
+        if (_username.getText().toString().equals("admin") &&
+                _password.getText().toString().equals("admin"))
+        {
+            Toast.makeText(getApplicationContext(), "Hello admin!",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Seems like you 're not admin!",
+                    Toast.LENGTH_SHORT).show();
+            _numberOfRemainingLoginAttempts--;
+            _attemptsLeftTV.setVisibility(View.VISIBLE);
+            _numberOfRemainingLoginAttemptsTV.setVisibility(View.VISIBLE);
+            _numberOfRemainingLoginAttemptsTV.setText(Integer.toString(_numberOfRemainingLoginAttempts));
+
+            if (_numberOfRemainingLoginAttempts == 0)
+            {
+                _login.setEnabled(false);
+                _loginLockedTV.setVisibility(View.VISIBLE);
+                _loginLockedTV.setBackgroundColor(Color.RED);
+                _loginLockedTV.setText("LOGIN LOCKED!!!");
+            }
+        }
+
     }
+    private void setupVariables() {
+        _username = (EditText) findViewById(R.id.usernameET);
+        _password = (EditText) findViewById(R.id.passwordET);
+        _login = (Button) findViewById(R.id.loginBtn);
+        _loginLockedTV = (TextView) findViewById(R.id.loginLockedTV);
+        _attemptsLeftTV = (TextView) findViewById(R.id.attemptsLeftTV);
+        _numberOfRemainingLoginAttemptsTV = (TextView) findViewById(R.id.numberOfRemainingLoginAttemptsTV);
+        _numberOfRemainingLoginAttemptsTV.setText(Integer.toString(_numberOfRemainingLoginAttempts));
+    }
+
+    public void createAccount(View view)
+    {
+        Toast.makeText(this, "You clicked the create account button!", Toast.LENGTH_SHORT).show();
+    }
+
 }
