cecb3850135f8254b3afcb867961fe81ed21cccc
killbot <ruben_gottardi@yahoo.com>
2014-12-12 18:48:20 -0700

    Other than iml issue should be done

--- app/src/main/java/edu/utah/cs4962/golfleaderboard/PlayerParValueView.java
+++ app/src/main/java/edu/utah/cs4962/golfleaderboard/PlayerParValueView.java
@@ -71,51 +71,6 @@ public class PlayerParValueView extends Fragment
         return v;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.login)
-        {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.create_account)
-        {
-            Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.join_tournament)
-        {
-            Intent intent = new Intent(getApplicationContext(), JoinTournament.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.create_tournament)
-        {
-            Intent intent = new Intent(getApplicationContext(), CreateTournament.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.edit_account)
-        {
-            Intent intent = new Intent(getApplicationContext(), EditAccount.class);
-            startActivity(intent);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
     @Override
